206273998f1613281a86da2914009656819224d4
prdn <paolo.ardoino@gmail.com>
2019-01-20 20:56:31 +0000

    format change

--- book.js
+++ book.js
@@ -9,15 +9,17 @@ const nBN = v => {
 const MSG_TYPES = {
   t_bu:
   4 + // SYM
+  1 + // ACTIVE
+  8 + // ID
   8 + // PRICE
-  8 + // CNT
   8 +  // AMOUNT
   1, // SIGN
   f_bu:
   4 + // SYM
+  1 + // ACTIVE
+  1 + // ID
   8 + // RATE
-  2 + // PERIOD
-  4 + // CNT
+  1 + // PERIOD
   8 + // AMOUNT
   1 // SIGN
 }
@@ -49,14 +51,19 @@ const fBookEntry = (symType, symId, e, seq) => {
   b.writeUInt8(msize, 1)
   b.fill((new UInt64BE(seq)).toBuffer(), 2)
   b.writeUInt32BE(symId, 10)
+  b.writeUInt8(e[6], 14)
 
   if (symType === 'f') {
-
+    b.fill((new UInt64BE(e[0])).toBuffer(), 15)
+    b.fill((new UInt64BE(nBN(e[3]).times(libCommon.DEF_MULTIPLIER).dp(0).toString(16), 16)).toBuffer(), 23)
+    b.fill((new UInt8(e[2])).toBuffer(), 31)
+    b.fill((new UInt64BE(nBN(e[4]).times(libCommon.DEF_MULTIPLIER).abs().dp(0).toString(16), 16)).toBuffer(), 32)
+    b.writeUInt8(e[3] >= 0 ? 0 : 1, 40)
   } else {
-    b.fill((new UInt64BE(e[1])).toBuffer(), 14)
-    b.fill((new UInt64BE(nBN(e[3]).times(libCommon.DEF_MULTIPLIER).dp(0).toString(16), 16)).toBuffer(), 22)
-    b.fill((new UInt64BE(nBN(e[4]).times(libCommon.DEF_MULTIPLIER).abs().dp(0).toString(16), 16)).toBuffer(), 30)
-    b.writeUInt8(e[4] >= 0 ? 0 : 1, 38)
+    b.fill((new UInt64BE(e[0])).toBuffer(), 15)
+    b.fill((new UInt64BE(nBN(e[1]).times(libCommon.DEF_MULTIPLIER).dp(0).toString(16), 16)).toBuffer(), 23)
+    b.fill((new UInt64BE(nBN(e[3]).times(libCommon.DEF_MULTIPLIER).abs().dp(0).toString(16), 16)).toBuffer(), 31)
+    b.writeUInt8(e[3] >= 0 ? 0 : 1, 39)
   }
 
   return b
